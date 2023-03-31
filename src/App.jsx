698f47382c3c68f3ea5840e7afc90f691b124f13
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-03-31 13:01:19 +0600

    create header component and added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello from Tailwind</h1>
+        <Header></Header>
     </div>
   );
 };
